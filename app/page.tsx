036cc77c4a0542a38ad9d6775b29c3a86d2af12d
Adithiyan <adithiyanr2@gmail.com>
2025-06-01 20:08:54 +0530

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <NavbarComp />
       <SeeOffers />
       <HeroSection />
@@ -26,6 +26,6 @@ export default function Home() {
       <Special />
       <QuickLinks />
       <Footer />
-    </div>
+    </>
   );
 }
