0ae3ff11e199d19256b6a2b6b2b56558ceda86ec
Adithiyan <adithiyanr2@gmail.com>
2025-06-02 15:18:50 +0530

    nav commit

--- app/components/NavbarComp.tsx
+++ app/components/NavbarComp.tsx
@@ -1,11 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { BsBag } from "react-icons/bs";
+import { BsArrowRight, BsBag, BsBoxSeam } from "react-icons/bs";
 import { FaApple } from "react-icons/fa";
 import { HiOutlineMenuAlt4, HiOutlineSearch } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
 import { motion } from "framer-motion";
 import { RiArrowLeftWideFill } from "react-icons/ri";
+import Link from "next/link";
+import { GoSearch } from "react-icons/go";
+import { PiBookmarkSimple, PiGearFine } from "react-icons/pi";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { SUB_MENU_LIST } from "@/constants/data";
 
 const sidebarVariants = {
   open: (height = 1000) => ({
@@ -45,229 +50,38 @@ const itemVariants = {
 };
 
 const NavbarComp = () => {
+  const MotionLink = motion(Link);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSubOpen, setIsSubOpen] = useState(false);
   const [submenu, setSubMenu] = useState<number>(0);
-
-  const subMenuList = [
-    {
-      title: "Shop",
-      categories: [
-        "Shop the Latest",
-        "Mac",
-        "iPad",
-        "iPhone",
-        "Apple Watch",
-        "Accessories",
-      ],
-      quickLinks: [
-        "Quick Links",
-        "Find a Store",
-        "Order Status",
-        "Way to Buy",
-        "Personal Setup",
-      ],
-      specialStores: ["Shop Special Stores", "Education", "Business"],
-    },
-    {
-      title: "Mac",
-      categories: [
-        "Explore All Mac",
-        "MacBook Air",
-        "MacBook Pro",
-        "iMac",
-        "Mac mini",
-        "Mac Studio",
-      ],
-      quickLinks: ["Shop Mac", "Shop Mac", "Mac Accessories", "Ways to Buy"],
-      specialStores: [
-        "Mac Support",
-        "AppleCare+ for Mac",
-        "MacOS Sequoia",
-        "Apple Intelligence",
-        "Apps by Apple",
-        "Continuity",
-        "iCloud+",
-      ],
-    },
-    {
-      title: "iPad",
-      categories: [
-        "Explore All iPad",
-        "iPad Pro",
-        "iPad Air",
-        "iPad",
-        "iPad mini",
-        "Apple Pencil",
-      ],
-      quickLinks: ["Shop iPad", "Shop iPad", "iPad Accessories", "Ways to Buy"],
-      specialStores: [
-        "More from iPad",
-        "iPad Support",
-        "AppleCare+ fro iPad",
-        "Apple Intelligence",
-        "Apps by Apple",
-        "Continuity",
-        "iCloud+",
-      ],
-    },
-    {
-      title: "iPhone",
-      categories: [
-        "Explore All iPhone",
-        "iPhone 16 Pro",
-        "iPhone 16",
-        "iPhone 16e",
-        "iPhone 15",
-      ],
-      quickLinks: [
-        "Shop iPhone",
-        "Shop iPhone",
-        "iPhone Accessories",
-        "Apple Trade In",
-      ],
-      specialStores: [
-        "More from iPhone",
-        "iPhone Support",
-        "AppleCare+ fro iPhone",
-        "Apple Intelligence",
-        "Apps by Apple",
-        "iCloud+",
-        "Siri",
-      ],
-    },
-    {
-      title: "Watch",
-      categories: [
-        "Explore All Apple Watch",
-        "Apple Watch Series 10",
-        "Apple Watch Ultra 2",
-        "Apple Watch SE",
-        "Apple Watch Nike",
-      ],
-      quickLinks: [
-        "Shop Watch",
-        "Shop Apple Watch",
-        "Apple Watch Straps",
-        "Apple Watch Accessories",
-      ],
-      specialStores: [
-        "More from Watch",
-        "Apple Watch Support",
-        "AppleCare+",
-        "Apple Watch For Your kids",
-        "Apps by Apple",
-      ],
-    },
-    {
-      title: "AirPods",
-      categories: [
-        "Explore All AirPods",
-        "AirPods 4",
-        "AirPods Pro 2",
-        "AirPods Max",
-      ],
-      quickLinks: ["Shop AirPods", "Shop AirPods", "AirPods Accessories"],
-      specialStores: [
-        "More from AirPods",
-        "AirPods Support",
-        "AppleCare+ for HeadPhones",
-        "Apple Music",
-      ],
-    },
-    {
-      title: "Tv & Home",
-      categories: [
-        "Explore TV & Home",
-        "Apple TV 4K",
-        "HomePod",
-        "HomePod mini",
-      ],
-      quickLinks: [
-        "Shop TV & Home",
-        "Shop Apple TV 4K",
-        "Shop HomePod",
-        "Shop HomePod mini",
-        "Shop Siri Remote",
-      ],
-      specialStores: [
-        "More from TV & Home",
-        "Apple TV Support",
-        "HomePod Support",
-        "AppleCare+",
-        "Apple TV+",
-        "Home app",
-        "Apple Music",
-        "Siri",
-      ],
-    },
-    {
-      title: "Entertainment",
-      categories: [
-        "Explore Entertainment",
-        "Apple One",
-        "Apple TV+",
-        "Apple Music",
-        "Apple Arcade",
-        "Apple Podcasts",
-        "Apple Books",
-        "Apple Store",
-      ],
-      quickLinks: ["Support", "Apple TV+ Support", "Apple Music Support"],
-      specialStores: [""],
-    },
-    {
-      title: "Accessories",
-      categories: [
-        "Shop All Accessories",
-        "Mac",
-        "iPad",
-        "iPhone",
-        "Apple Watch",
-        "AirPods",
-        "TV & Home",
-      ],
-      quickLinks: [
-        "Explore Accessories",
-        "Made by Apple",
-        "Beats by Dr. Dre",
-        "AirTag",
-      ],
-      specialStores: [""],
-    },
-    {
-      title: "Support",
-      categories: ["iPhone", "Mac", "iPad", "Watch", "AirPods", "Music", "TV"],
-      quickLinks: ["Get Help", "Community", "Check Coverage", "Repair"],
-      specialStores: [
-        "Helpful Topics",
-        "Get AppleCare+",
-        "Apple Account and Password",
-        "Billing & Subscriptions",
-        "Accessibility",
-      ],
-    },
-  ];
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isBagOpen, setIsBagOpen] = useState(false);
 
   useEffect(() => {
-    if (isMenuOpen) {
+    if (isSearchOpen || isBagOpen || isMenuOpen) {
       document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "";
+      document.body.style.overflow = "auto";
     }
 
     return () => {
-      document.body.style.overflow = "";
+      document.body.style.overflow = "auto";
     };
-  }, [isMenuOpen]);
+  }, [isSearchOpen, isBagOpen, isMenuOpen]);
 
   return (
     <div className="h-16 p-5 relative">
       <div className="flex justify-between items-center">
         <FaApple className="size-5.5" />
         <div className="flex flex-row gap-7 items-center">
-          <HiOutlineSearch className="size-5 text-black/70" />
-          <BsBag className="size-4 text-black/70" />
+          <HiOutlineSearch
+            onClick={() => setIsSearchOpen(true)}
+            className="size-5 text-black/70"
+          />
+          <BsBag
+            onClick={() => setIsBagOpen(true)}
+            className="size-4 text-black/70"
+          />
           <HiOutlineMenuAlt4
             onClick={() => setIsMenuOpen(true)}
             className="size-5 text-black/70"
@@ -353,55 +167,198 @@ const NavbarComp = () => {
                   transition: { staggerChildren: 0.03, staggerDirection: -1 },
                 },
               }}
-              className="mt-4 flex pl-10 max-w-[20rem] overscroll-auto flex-col gap-2"
+              className="mt-4 flex pl-10 max-w-[20rem] overscroll-auto  flex-col gap-2"
             >
-              {subMenuList[submenu]?.categories.map((item, idx) => (
-                <motion.h2
+              {SUB_MENU_LIST[submenu].categories.map((item, idx) => (
+                <MotionLink
                   key={idx}
+                  href={item.link}
                   variants={itemVariants}
                   className="text-[27px] font-Sf-semibold text-black/80"
                 >
-                  {item}
-                </motion.h2>
+                  {item.title}
+                </MotionLink>
               ))}
-              <div className="mt-[1.5rem]">
-                {subMenuList[submenu]?.quickLinks.map((item, idx) => (
+              <div className="mt-[1rem]">
+                {SUB_MENU_LIST[submenu].quickLinks.map((item, idx) => (
                   <div key={idx} className="flex flex-col gap-3">
                     <motion.h3
                       variants={itemVariants}
                       className="text-[18px] text-black/60"
                     >
-                      {idx === 0 ? item : ""}
+                      {idx === 0 ? item.title : ""}
                     </motion.h3>
-                    <motion.h2
+                    <MotionLink
+                      href={idx !== 0 ? item.link : ""}
                       variants={itemVariants}
                       className="text-[16px] font-Sf-semibold text-black/80"
                     >
-                      {idx !== 0 ? item : ""}
-                    </motion.h2>
+                      {idx !== 0 ? item.title : ""}
+                    </MotionLink>
                   </div>
                 ))}
               </div>
-              <div className="mt-[1.5rem]">
-                {subMenuList[submenu]?.specialStores.map((item, idx) => (
+              <div className="mt-[1rem]">
+                {SUB_MENU_LIST[submenu].specialStores.map((item, idx) => (
                   <div key={idx} className="flex flex-col gap-3">
                     <motion.h3
                       variants={itemVariants}
                       className="text-[18px] text-black/60"
                     >
-                      {idx === 0 ? item : ""}
+                      {idx === 0 ? item.title : ""}
                     </motion.h3>
-                    <motion.h2
+                    <MotionLink
+                      href={idx !== 0 ? item.link : ""}
                       variants={itemVariants}
                       className="text-[16px] font-Sf-semibold text-black/80"
                     >
-                      {idx !== 0 ? item : ""}
-                    </motion.h2>
+                      {idx !== 0 ? item.title : ""}
+                    </MotionLink>
                   </div>
                 ))}
               </div>
             </motion.div>
           )}
+        </motion.div>
+      )}
+      {isSearchOpen && (
+        <motion.div
+          initial="closed"
+          animate="open"
+          exit="closed"
+          variants={sidebarVariants}
+          className="fixed w-full h-screen top-0 left-0 pl-4 pr-5 pt-5 bg-white z-50"
+        >
+          <div className={`flex  items-center justify-end `}>
+            <IoClose
+              className="size-8 text-black/70"
+              onClick={() => {
+                setIsSearchOpen(false);
+              }}
+            />
+          </div>
+          <motion.div
+            initial="closed"
+            animate="open"
+            variants={{
+              open: {
+                transition: { staggerChildren: 0.05, delayChildren: 0.2 },
+              },
+              closed: {
+                transition: { staggerChildren: 0.03, staggerDirection: -1 },
+              },
+            }}
+            className={`mt-2 pl-10 flex flex-col gap-2 ${
+              isSearchOpen ? "block" : "hidden"
+            }`}
+          >
+            <div className="flex gap-2 items-center">
+              <GoSearch className="size-6 mb-1 text-black/40" />
+              <input
+                type="text"
+                placeholder="Search"
+                className="bg-transparent outline-none placeholder:font-Sf-semibold placeholder:text-xl placeholder:text-gray-600 "
+              />
+            </div>
+            <div className="mt-7">
+              <h2 className="text-lg text-black/50 mb-3">Quick Links</h2>
+              {[
+                { title: "Find a Store", link: "/mac" },
+                { title: "Accessories", link: "/mac" },
+                { title: "AirPods", link: "/mac" },
+                { title: "Apple Intelligence", link: "/mac" },
+                { title: "Apple Trade In", link: "/mac" },
+              ].map((item, idx) => (
+                <MotionLink
+                  href={item.link}
+                  key={idx}
+                  variants={itemVariants}
+                  className="flex mb-3 items-center gap-2"
+                >
+                  <BsArrowRight />
+                  <motion.h2 className="text-[18px] font-Sf-semibold text-black/80">
+                    {item.title}
+                  </motion.h2>
+                </MotionLink>
+              ))}
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+      {isBagOpen && (
+        <motion.div
+          initial="closed"
+          animate="open"
+          exit="closed"
+          variants={sidebarVariants}
+          className="fixed w-full h-screen top-0 left-0 pl-4 pr-5 pt-5 bg-white z-50"
+        >
+          <div className={`flex  items-center justify-end `}>
+            <IoClose
+              className="size-8 text-black/70"
+              onClick={() => {
+                setIsBagOpen(false);
+              }}
+            />
+          </div>
+          <motion.div
+            initial="closed"
+            animate="open"
+            variants={{
+              open: {
+                transition: { staggerChildren: 0.05, delayChildren: 0.2 },
+              },
+              closed: {
+                transition: { staggerChildren: 0.03, staggerDirection: -1 },
+              },
+            }}
+            className={`mt-2 pl-7 flex flex-col gap-2 ${
+              isBagOpen ? "block" : "hidden"
+            }`}
+          >
+            <div className="flex flex-col gap-2">
+              <h2 className="font-Sf-semibold mb-4 text-3xl text-black/70">
+                Your Bag is empty.
+              </h2>
+              <h2 className="text-lg text-black/50">
+                <span className="text-blue-500 underline">Sign in</span> to see
+                if you have any saved <br /> items
+              </h2>
+            </div>
+            <div className="mt-7">
+              <h2 className="text-lg text-black/50 mb-3">My Profile</h2>
+              {[
+                {
+                  title: "Orders",
+                  link: "/mac",
+                  icon: <BsBoxSeam className="size-3" />,
+                },
+                {
+                  title: "Your Saves",
+                  link: "/mac",
+                  icon: <PiBookmarkSimple />,
+                },
+                { title: "Account", link: "/mac", icon: <PiGearFine /> },
+                {
+                  title: "Sign in",
+                  link: "/mac",
+                  icon: <FaRegCircleUser className="size-3" />,
+                },
+              ].map((item, idx) => (
+                <MotionLink
+                  href={item.link}
+                  key={idx}
+                  variants={itemVariants}
+                  className="flex mb-3 items-center gap-4"
+                >
+                  {item.icon}
+                  <motion.h2 className="text-[18px] font-Sf-semibold text-black/80">
+                    {item.title}
+                  </motion.h2>
+                </MotionLink>
+              ))}
+            </div>
+          </motion.div>
         </motion.div>
       )}
     </div>
---
+++ constants/data.ts
@@ -0,0 +1,645 @@
+const SUB_MENU_LIST = [
+  {
+    categories: [
+      {
+        title: "Shop & Latest",
+        link: "/mac",
+      },
+      {
+        title: "Mac",
+        link: "/mac",
+      },
+      {
+        title: "iPad",
+        link: "/mac",
+      },
+      {
+        title: "iPhone",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch",
+        link: "/mac",
+      },
+      {
+        title: "Accessories",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Quick Links",
+        link: "/mac",
+      },
+      {
+        title: "Find a Store",
+        link: "/mac",
+      },
+      {
+        title: "Order Status",
+        link: "/mac",
+      },
+      {
+        title: "Way to Buy",
+        link: "/mac",
+      },
+      {
+        title: "Personal Setup",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      { title: "Shop Special Stores", link: "/mac" },
+      { title: "Education", link: "/mac" },
+      { title: "Business", link: "/mac" },
+    ],
+  },
+  {
+    categories: [
+      {
+        title: "Explore All Mac",
+        link: "/mac",
+      },
+      {
+        title: "MacBook Air",
+        link: "/mac",
+      },
+      {
+        title: "MacBook Pro",
+        link: "/mac",
+      },
+      {
+        title: "iMac",
+        link: "/mac",
+      },
+      {
+        title: "Mac mini",
+        link: "/mac",
+      },
+      {
+        title: "Mac Studio",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Shop Mac",
+        link: "/mac",
+      },
+      {
+        title: "Shop Mac",
+        link: "/mac",
+      },
+      {
+        title: "Mac Accessories",
+        link: "/mac",
+      },
+      {
+        title: "Ways to Buy",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      {
+        title: "Mac Support",
+        link: "/mac",
+      },
+      {
+        title: "AppleCare+ for Mac",
+        link: "/mac",
+      },
+      {
+        title: "MacOS Sequoia",
+        link: "/mac",
+      },
+      {
+        title: "Apple Intelligence",
+        link: "/mac",
+      },
+      {
+        title: "Apps by Apple",
+        link: "/mac",
+      },
+      {
+        title: "Continuity",
+        link: "/mac",
+      },
+      {
+        title: "iCloud+",
+        link: "/mac",
+      },
+    ],
+  },
+  {
+    categories: [
+      {
+        title: "Explore All iPad",
+        link: "/mac",
+      },
+      {
+        title: "iPad Pro",
+        link: "/mac",
+      },
+      {
+        title: "iPad Air",
+        link: "/mac",
+      },
+      {
+        title: "iPad",
+        link: "/mac",
+      },
+      {
+        title: "iPad mini",
+        link: "/mac",
+      },
+      {
+        title: "Apple Pencil",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Shop iPad",
+        link: "/mac",
+      },
+      {
+        title: "Shop iPad",
+        link: "/mac",
+      },
+      {
+        title: "iPad Accessories",
+        link: "/mac",
+      },
+      {
+        title: "Ways to Buy",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      {
+        title: "More from iPad",
+        link: "/mac",
+      },
+      {
+        title: "iPad Support",
+        link: "/mac",
+      },
+      {
+        title: "AppleCare+ for iPad",
+        link: "/mac",
+      },
+      {
+        title: "Apple Intelligence",
+        link: "/mac",
+      },
+      {
+        title: "Apps by Apple",
+        link: "/mac",
+      },
+      {
+        title: "Continuity",
+        link: "/mac",
+      },
+      {
+        title: "iCloud+",
+        link: "/mac",
+      },
+    ],
+  },
+  {
+    categories: [
+      {
+        title: "Explore All iPhone",
+        link: "/mac",
+      },
+      {
+        title: "iPhone 16 Pro",
+        link: "/mac",
+      },
+      {
+        title: "iPhone 16",
+        link: "/mac",
+      },
+      {
+        title: "iPhone 16e",
+        link: "/mac",
+      },
+      {
+        title: "iPhone 15",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Shop iPhone",
+        link: "/mac",
+      },
+      {
+        title: "Shop iPhone",
+        link: "/mac",
+      },
+      {
+        title: "iPhone Accessories",
+        link: "/mac",
+      },
+      {
+        title: "Apple Trade In",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      {
+        title: "More from iPhone",
+        link: "/mac",
+      },
+      {
+        title: "iPhone Support",
+        link: "/mac",
+      },
+      {
+        title: "AppleCare+ fro iPhone",
+        link: "/mac",
+      },
+      {
+        title: "Apple Intelligence",
+        link: "/mac",
+      },
+      {
+        title: "Apps by Apple",
+        link: "/mac",
+      },
+      {
+        title: "iCloud+",
+        link: "/mac",
+      },
+      {
+        title: "Siri",
+        link: "/mac",
+      },
+    ],
+  },
+  {
+    categories: [
+      {
+        title: "Explore All Apple Watch",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch Series 10",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch Ultra 2",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch SE",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch Nike",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Shop Watch",
+        link: "/mac",
+      },
+      {
+        title: "Shop Apple Watch",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch Straps",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch Accessories",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      {
+        title: "More from Watch",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch Support",
+        link: "/mac",
+      },
+      {
+        title: "AppleCare+",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch For Your kids",
+        link: "/mac",
+      },
+      {
+        title: "Apps by Apple",
+        link: "/mac",
+      },
+    ],
+  },
+  {
+    categories: [
+      {
+        title: "Explore All AirPods",
+        link: "/mac",
+      },
+      {
+        title: "AirPods 4",
+        link: "/mac",
+      },
+      {
+        title: "AirPods Pro 2",
+        link: "/mac",
+      },
+      {
+        title: "AirPods Max",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Shop AirPods",
+        link: "/mac",
+      },
+      {
+        title: "Shop AirPods",
+        link: "/mac",
+      },
+      {
+        title: "AirPods Accessories",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      {
+        title: "More from AirPods",
+        link: "/mac",
+      },
+      {
+        title: "AirPods Support",
+        link: "/mac",
+      },
+      {
+        title: "AppleCare+ for HeadPhones",
+        link: "/mac",
+      },
+      {
+        title: "Apple Music",
+        link: "/mac",
+      },
+    ],
+  },
+  {
+    categories: [
+      {
+        title: "Explore TV & Home",
+        link: "/mac",
+      },
+      {
+        title: "Apple TV 4K",
+        link: "/mac",
+      },
+      {
+        title: "HomePod",
+        link: "/mac",
+      },
+      {
+        title: "HomePod mini",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Shop TV & Home",
+        link: "/mac",
+      },
+      {
+        title: "Shop Apple TV 4K",
+        link: "/mac",
+      },
+      {
+        title: "Shop HomePod",
+        link: "/mac",
+      },
+      {
+        title: "Shop HomePod mini",
+        link: "/mac",
+      },
+      {
+        title: "Shop Siri Remote",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      {
+        title: "More from TV & Home",
+        link: "/mac",
+      },
+      {
+        title: "Apple TV Support",
+        link: "/mac",
+      },
+      {
+        title: "HomePod Support",
+        link: "/mac",
+      },
+      {
+        title: "AppleCare+",
+        link: "/mac",
+      },
+      {
+        title: "Apple TV+",
+        link: "/mac",
+      },
+      {
+        title: "Home app",
+        link: "/mac",
+      },
+      {
+        title: "Apple Music",
+        link: "/mac",
+      },
+      {
+        title: "Siri",
+        link: "/mac",
+      },
+    ],
+  },
+  {
+    categories: [
+      {
+        title: "Explore Entertainment",
+        link: "/mac",
+      },
+      {
+        title: "Apple One",
+        link: "/mac",
+      },
+      {
+        title: "Apple TV+",
+        link: "/mac",
+      },
+      {
+        title: "Apple Music",
+        link: "/mac",
+      },
+      {
+        title: "Apple Arcade",
+        link: "/mac",
+      },
+      {
+        title: "Apple Podcasts",
+        link: "/mac",
+      },
+      {
+        title: "Apple Books",
+        link: "/mac",
+      },
+      {
+        title: "Apple Store",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Support",
+        link: "/mac",
+      },
+      {
+        title: "Apple TV+ Support",
+        link: "/mac",
+      },
+      {
+        title: "Apple Music Support",
+        link: "/mac",
+      },
+    ],
+    specialStores: [{ title: "", link: "" }],
+  },
+  {
+    categories: [
+      {
+        title: "Shop All Accessories",
+        link: "/mac",
+      },
+      {
+        title: "Mac",
+        link: "/mac",
+      },
+      {
+        title: "iPad",
+        link: "/mac",
+      },
+      {
+        title: "iPhone",
+        link: "/mac",
+      },
+      {
+        title: "Apple Watch",
+        link: "/mac",
+      },
+      {
+        title: "AirPods",
+        link: "/mac",
+      },
+      {
+        title: "TV & Home",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Explore Accessories",
+        link: "/mac",
+      },
+      {
+        title: "Made by Apple",
+        link: "/mac",
+      },
+      {
+        title: "Beats by Dr. Dre",
+        link: "/mac",
+      },
+      {
+        title: "AirTag",
+        link: "/mac",
+      },
+    ],
+    specialStores: [{ title: "", link: "" }],
+  },
+  {
+    categories: [
+      {
+        title: "iPhone",
+        link: "/mac",
+      },
+      {
+        title: "Mac",
+        link: "/mac",
+      },
+      {
+        title: "iPad",
+        link: "/mac",
+      },
+      {
+        title: "Watch",
+        link: "/mac",
+      },
+      {
+        title: "Music",
+        link: "/mac",
+      },
+      {
+        title: "TV",
+        link: "/mac",
+      },
+    ],
+    quickLinks: [
+      {
+        title: "Get Help",
+        link: "/mac",
+      },
+      {
+        title: "Community",
+        link: "/mac",
+      },
+      {
+        title: "Check Coverage",
+        link: "/mac",
+      },
+      {
+        title: "Repair",
+        link: "/mac",
+      },
+    ],
+    specialStores: [
+      {
+        title: "Helpful Topics",
+        link: "/mac",
+      },
+      {
+        title: "Get AppleCare+",
+        link: "/mac",
+      },
+      {
+        title: "Apple Account and Password",
+        link: "/mac",
+      },
+      {
+        title: "Billing & Subscriptions",
+        link: "/mac",
+      },
+      {
+        title: "Accessibility",
+        link: "/mac",
+      },
+    ],
+  },
+];
+
+export { SUB_MENU_LIST };
